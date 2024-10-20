3f6e0a488b63b63ad5e333301cc992efb8cb6cd0
Alex <alexandre.arabian.j@gmail.com>
2024-10-20 03:09:45 +0200

    Logout fix 1

--- src/components/BurgerMenu.jsx
+++ src/components/BurgerMenu.jsx
@@ -2,7 +2,7 @@ import { faPowerOff, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
-export default function BurgerMenu(handleLogout) {
+export default function BurgerMenu({handleLogout}) {
 
     return (
         <>
@@ -25,7 +25,7 @@ export default function BurgerMenu(handleLogout) {
                             <li>
                                 <FontAwesomeIcon icon={faGithub} style={{ marginRight: '10px' }} /> More cool stuff</li>
                         </a>
-                        <a href="#" onClick={handleLogout}>
+                        <a href="#" onClick={handleLogout()}>
                             <li>
                                 <FontAwesomeIcon icon={faPowerOff} style={{ marginRight: '10px' }} /> Logout
                             </li>
